d9d9f0454d96ea5732b72fcad9e03095853cf9b2
Javi Gauxachs <javi.gauxachs@gmail.com>
2023-04-22 16:52:53 +0200

    cambio de nombre de archivos

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 import { writeFile } from "fs/promises";
 import { render } from "./render.js";
-import { loadUsers } from "./users.js";
+import { loadCountries } from "./Countriees.js";
+import { loadCountry } from "./country.js";
 
-const users = await loadUsers(100);
-const html = render(users);
-await writeFile('users.html', html);
-
+const countries = await loadCountries();
+const html = render(countries, "");
+await writeFile('countries.html', html);
