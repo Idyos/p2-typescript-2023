97d2b7dde10b06d283eab9df2a24a0197afce5be
Javi Gauxachs <javi.gauxachs@gmail.com>
2023-04-23 21:39:59 +0200

    añadido loop para crear page de cada país

--- main.ts
+++ main.ts
@@ -1,8 +1,16 @@
 import { writeFile } from "fs/promises";
-import { render } from "./render.js";
+import { countryRender, render } from "./render.js";
 import { loadCountries } from "./Countriees.js";
 import { loadCountry } from "./country.js";
 
 const countries = await loadCountries();
-const html = render(countries, "");
+const html = render(countries);
 await writeFile('countries.html', html);
+
+ for(const countryIndividual of countries){
+    const country = await loadCountry(countryIndividual.name.common);
+    const html = countryRender(country); 
+    await writeFile(`./countries/${countryIndividual.name.common}.html`, html);
+}
+
+
