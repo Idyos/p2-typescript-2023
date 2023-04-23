9f931b37d82e3d74317b517c03698f6c43fd95ab
Javi Gauxachs <javi.gauxachs@gmail.com>
2023-04-23 21:40:24 +0200

    añadido el render de la página para paises individualmente

--- render.ts
+++ render.ts
@@ -1,5 +1,8 @@
+import { type } from "os";
 import { Countries } from "./Countriees.js";
 import { Country } from "./country.js";
+import { count } from "console";
+import { cursorTo } from "readline";
 
 const head = (title: string) => `
 <head>
@@ -70,42 +73,164 @@ const head = (title: string) => `
   </style>
 </head>`;
 
+const headCountry = (title: string) => `<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>${title}</title>
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+
+img {
+  height: 30vh;
+}
+
+.jumbotron {
+  font-family: sans-serif;
+  padding: 20px;
+  box-shadow: 0px 8px 20px 1px #0000008f;
+  display: flex;
+  flex-direction: row;
+}
+
+.countryInfo{
+  margin-left: 20px;
+  column-gap: 50px;
+  grid-template-columns: repeat(2, minmax(200px, 1fr));
+  display: grid;
+  align-items: center;
+}
+
+iframe{
+  width: 100%;
+  height: 70%;
+}
+</style>
+</head>`;
+
 const renderCountries = (countries: Array<Countries>) => {
   let html = "";
   for (const country of countries) {
-    html += `<div class="country">
+    html += `<a class="country" href="./countries/${country.name.common}.html">
       <img src="${country.flags.png}" />
       <div class="data">
         <div class="name">${country.name.common}</div>
         <div class="info"><b>Capital:</b> ${country.capital} <b>Continent:</b> ${country.region}</div>
       </div>
-    </div>`;
+    </a>`;
   }
   return html;
-}
+};
 
-const renderCountry = (name: string) => {
-  let html = "";
-  for (const country of name) {
-    html += `<div class="country">
-      <img src="" />
-      <div class="data">
-        <div class="name"></div>
-        <div class="info"><b>Capital:</b>  <b>Continent:</b> </div>
-      </div>
-    </div>`;
+//<p><b>Currency: </b>${country[0].currencies} ${country[0].currencies}</p>
+
+const getLanguages = (languages: object) => {
+  let languagesString = "";
+  let index = 0;
+  if (languages != undefined && languages != null) {
+    for (const language of Object.values(languages)) {
+      if (index == Object.values(languages).length - 1)
+        languagesString += `<p style="display: inline-block;">${language}</p>`;
+      else
+        languagesString += `<p style="display: inline-block;">${language}, </p>`;
+      index++;
+    }
   }
-  return html;
-}
+  return languagesString;
+};
+
+const getCurrencies = (currencies: object) => {
+  let currenciesString = "";
+  let index = 0;
+  if (currencies != undefined && currencies != null) {
+    for (const currency of Object.values(currencies)) {
+      if (index == Object.values(currencies).length - 1)
+        currenciesString += `<p style="display: inline-block; margin: 0">${currency.name} <b>(${currency.symbol})</b></p>`;
+      else
+        currenciesString += `<p style="display: inline-block; margin: 0">${currency.name} <b>(${currency.symbol})</b>, </p>`;
+      index++;
+    }
+  }
+  return currenciesString;
+};
+
+const getTimezones = (timezones: Array<string>) => {
+  let timezonesString = "";
+  let index = 0;
+  for (const timezone of timezones) {
+    if (index == timezones.length - 1)
+      timezonesString += `<p style="display: inline-block; margin: 0">${timezone} <b>(${timezone})</b></p>`;
+    else
+      timezonesString += `<p style="display: inline-block; margin: 0">${timezone} <b>(${timezone})</b>, </p>`;
+    index++;
+  }
+  return timezonesString;
+};
+
+export const countryRender = (country: Array<Country>) => {
+  return `
+<html>
+  ${headCountry(country[0].name.common)}
+  <body>
+    <div class="jumbotron">
+      <img src="${country[0].flags.png}" />
+        <div class="countryInfo">
+          <h1 style="margin-top: 0;">${country[0].name.official}</h1>
+          <div class="monedas">
+          <h3 style="display: inline-block; margin: 0">Currencies: </h3>
+          ${getCurrencies(country[0].currencies)}
+        </div>
+          <p><b>Capital:</b> ${country[0].capital}</p>
+
+     
+        <div class="idiomas">
+          <h3 style="display: inline-block; margin: 0">Languages: </h3>
+          ${getLanguages(country[0].languages)}
+        </div>
+
+          <p><b>Population:</b> ${country[0].population}</p>
+
+          <p><b>Location:</b> ${country[0].region}, ${country[0].subregion}</p>
 
 
-export const render = (countries: Array<Countries>, name: string="") => {
+          <p><b>United Nations Member:</b> ${
+            country[0].unMember ? "Yes" : "No"
+          }</p>
+          
+          <div class="timezones">
+          <p style="display: inline-block; margin: 0"><b>Timezones:</b></p>
+          ${getTimezones(country[0].timezones)}
+          </div>
+
+          <p><b>Area:</b> ${country[0].area}m<sup>2</sup></p>
+
+        </div>
+    </div>
+    <iframe 
+  frameborder="0" 
+  scrolling="no" 
+  marginheight="0" 
+  marginwidth="0" 
+  src=https://maps.google.com/maps?q=${country[0].latlng[0]},${
+    country[0].latlng[1]
+  }&hl=en&z=9&amp;output=embed
+
+ </iframe>
+
+  </body>
+</html>`;
+};
+
+export const render = (countries: Array<Countries>) => {
   return `
 <html>
   ${head("All countries in the world")}
   <body>
   <div class="grid">
-  ${countries.length!==0 ? renderCountries(countries) : renderCountry(name)}
+  ${renderCountries(countries)}
   </body>
   </div>
 </html>`;
